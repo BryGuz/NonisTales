cc14bbf89391e5270a8c1997f61869778144791b
Bryan Guzmán <bryan.guzman@software-colombia.com>
2023-10-04 00:59:20 -0500

    style: adjust title style

--- nonis-tales/src/App.tsx
+++ nonis-tales/src/App.tsx
@@ -4,11 +4,11 @@ import video from './assets/videos/background/bg.mp4'
 function App (): JSX.Element {
   return (
     <>
-    <div className='relative min-w-full min-h-screen overflow-hidden text-center flex items-center justify-center'>
+    <div className='relative min-w-full min-h-screen overflow-hidden text-center flex items-end justify-center'>
       <video className='videoTag absolute top-0 left-0 object-cover h-full w-full' autoPlay loop muted>
         <source src={video} type='video/mp4' />
       </video>
-      <h1 className='z-10 text-5xl sm:text-7xl text-white font-serif font-bold hover:text-cyan-700'>NONIS TALES</h1>
+      <h1 className='z-10 text-5xl sm:text-7xl text-white font-serif font-bold mb-20'>NONIS TALES</h1>
     </div>
     </>
   )
