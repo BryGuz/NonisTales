ef7cd495d9ced55be9b2a0e3960190e9dbb4ee85
Bryan Guzmán <bryan.guzman@software-colombia.com>
2023-10-04 00:52:19 -0500

    style: adjust some text issues

--- nonis-tales/src/App.tsx
+++ nonis-tales/src/App.tsx
@@ -8,7 +8,7 @@ function App (): JSX.Element {
       <video className='videoTag absolute top-0 left-0 object-cover h-full w-full' autoPlay loop muted>
         <source src={video} type='video/mp4' />
       </video>
-      <h1 className='z-10 sm:text-7xl md:text-9xl text-white font-serif font-bold hover:text-cyan-700'>NONIS TALES</h1>
+      <h1 className='z-10 text-5xl sm:text-7xl text-white font-serif font-bold hover:text-cyan-700'>NONIS TALES</h1>
     </div>
     </>
   )
